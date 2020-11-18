03ede919efe91dbb2195f6200e37bee3c20b2361
Bar Sela <Barsela03@gmail.com>
2020-11-18 23:26:56 +0200

    json.h library added

--- HR4U/main.cpp
+++ HR4U/main.cpp
@@ -1,14 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdlib.h>
 #include <iostream>
+#include <json\json.h>
 #include <json.hpp>
 
 using namespace jsoncons;
 using namespace std;
 int main()
 {
-	cout << "Hello World\n";
-	cout << "bar & bar test";
-	// hello
+	cout << "new library";
 	return 0;
 }
