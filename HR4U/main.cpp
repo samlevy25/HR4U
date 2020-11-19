1587120c4cffbf0733a2e7247e418958b2be18d2
Bar Sela <Barsela03@gmail.com>
2020-11-19 21:52:41 +0200

    update database

--- HR4U/main.cpp
+++ HR4U/main.cpp
@@ -5,17 +5,47 @@
 #include <json.hpp>
 #include <jsoncons_ext/jmespath/jmespath.hpp>
 #include<ctime>
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include <map>
+#include <jsoncons_ext/jsonpointer/jsonpointer.hpp>
+#include <fstream>
 
 using namespace jsoncons;
 using namespace std;
 
 
+void first_example_a()
+{
+	std::string path = "C:/Users/Bar Sela/source/repos/HR4U/HR4U/database.json";
+	std::fstream is(path);
+	if (!is)
+	{
+		std::cout << "Cannot open " << path << std::endl;
+		return;
+	}
+	json books = json::parse(is);
 
+	for (std::size_t i = 0; i < books.size(); ++i)
+	{
+		try
+		{
+			json& book = books[i];
+			std::string author = book["author"].as<std::string>();
+			std::string title = book["title"].as<std::string>();
+			double price = book["price"].as<double>();
+			std::cout << author << ", " << title << ", " << price << std::endl;
+		}
+		catch (const std::exception & e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
+}
 int main()
 {
-	cout << "Hello World\n";
-	cout << "bar & bar test";
-	// hello
+	first_example_a();
 	return 0;
 }
 
